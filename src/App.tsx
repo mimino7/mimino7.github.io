d5b8b10e20af453bbb510a6cc0101d77daae7b47
mimino7 <mimino7@mail.ru>
2022-05-31 17:26:21 +0300

    added_INTRO

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Intro from "./Components/Intro/Intro";
+import "./App.css";
+import { NAME_INTRO, PROF_INTRO, social } from "./DB";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cont__wrap">
+        <div className="content">
+          <div className="menu" style={{width: 90}}>
+            <div className="menu_content">
+              <div className="menu_icon">rrr</div>
+              <div className="menu_icon">rrr</div>
+              <div className="menu_icon">rrr</div>
+              <div className="menu_icon">rrr</div>
+            </div>
+          </div>
+          <Intro name={NAME_INTRO} prof={PROF_INTRO} social={social}/>
+          <div className="info" style={{width: 470}}>info</div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Types/types.tsx
@@ -0,0 +1,7 @@
+export interface ISocial {
+  phone: any,
+  telegram: any,
+  email: any,
+  github: any,
+
+}
